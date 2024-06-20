51c43d21fa2fc2ac54afd7031a760fc650488d15
Ravindu156 <puresoftlk@gmail.com>
2024-06-20 23:55:04 +0530

    initial commit

---
+++ MachineLearning For Project/main.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
