fa72e42da08b0c28fec27a1b9aaf4196c7acc31a
Pasindunuwan2000 <154910776+Pasindunuwan2000@users.noreply.github.com>
2024-06-20 23:56:28 +0530

    Update main.py

--- MachineLearning For Project/main.py
+++ MachineLearning For Project/main.py
@@ -3,3 +3,5 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
+import tkinter as tk
+from tkinter import ttk
